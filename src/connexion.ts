359a2651d64564f7e484aaa31aebbdd8a5c95a72
Francis Boily <francis.boily@axesnetwork.com>
2020-04-26 22:13:23 -0400

    Adding secure web socket.

--- src/connexion.ts
+++ src/connexion.ts
@@ -10,7 +10,7 @@ export default class Connexion {
     private constructor() {
         this._lastPingTimestamp = 0;
         this._latency = 0;
-        this._socket = io("ws://cryptic-atoll-71509.herokuapp.com", { transports: ["websocket"] });
+        this._socket = io("wss://cryptic-atoll-71509.herokuapp.com", { transports: ["websocket"] });
     }
 
     static getInstance(): Connexion {
